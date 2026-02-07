430c1907db73315ced5f272214103c952f58dd73
graziella01 <amberbambico.54@gmail.com>
2026-02-07 21:03:44 +0800

    feat: refactor AppLayout, Header, and Sidebar components to use styled-components for improved styling and structure

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,16 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-200);
+`;
+
 function Header() {
   return (
-    <header>
+    <StyledHeader>
       <p>HEADER</p>
-    </header>
+    </StyledHeader>
   );
 }
 
